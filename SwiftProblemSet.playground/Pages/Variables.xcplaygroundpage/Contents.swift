ad825e174e0c56e36ccd21c2255a6390c60feb41
aminhp93 <minhpn.org.ec@gmail.com>
2017-01-02 18:00:46 -0800

    adding first problem set

---
+++ SwiftProblemSet.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,46 @@
+/*:
+ [Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
+ ****
+ */
+//: ## Variables
+/*:
+ ### Exercise 1
+ 
+ For each of the following names and types, create a variable or constant that best represents it:
+ 
+ - gallonsRemainingInTank (`Double`)
+ - ageOfSpecimenInWeeks (`Int`)
+ - birthplace (`String`)
+ - myFirstProgrammingLanguage (`String`)
+ */
+
+//: ### Exercise 2
+//: Make the minimal changes necessary to fix the following compiler errors related to assignment, variables, and constants.
+var favoriteLanguage: Character = "S"
+var gallonsRemainingInTank = "2"
+var ageOfSpecimenInWeeks = 32.45
+let bestProgrammingLanguage: String = "myFirstProgrammingLanguage"
+
+//: ### Exercise 3
+//: Below is some code to reverse a string. We've declared `stringToReverse` and `arrayOfCharacters` as variables. Rewrite this code to reverse a string using only constants declared with `let`, no variables. **Note**: In the new version you may use as many constants as you'd like.
+let stringToReverse = "Mutable or Immutable? That is the question."
+let arrayOfCharacters = String(stringToReverse.characters.reversed())
+
+/*: 
+### Exercise 4
+
+ For the following descriptions, create a variable or constant that best represents it:
+
+- an `Int` value representing the hunger level of an animal over time.
+- an `Int` value representing the score always required to win a game.
+- a `String` value representing a student’s birthday.
+- a `String` value representing what a user types into the status update area.
+*/
+var hunger_level:Int = 5
+var score:Int = 10
+var birhday:String = "May 12"
+var re:String = "Dont know"
+/*:
+ ****
+ [Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
+ */
---
+++ swift_for_beginners.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+func calculateTip(tip:Double) -> Double{
+    return tip*12
+}
+
+calculateTip(tip: 12.0)
